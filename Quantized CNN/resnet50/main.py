2f023db219a2787f29888518994273a4ede86b86
Samira Nazari <125339896+nilay1400@users.noreply.github.com>
2024-05-01 10:44:31 +0330

    Update main.py

--- Quantized CNN/resnet50/main.py
+++ Quantized CNN/resnet50/main.py
@@ -51,47 +51,10 @@ trainset_1 = torch.utils.data.Subset(dataset, evens)
 
 data = val_dataloader()
 
-# import timeit
-# correct = 0
-# total = 0
-
-# model.eval()
-# start_time = timeit.default_timer()
-# with torch.no_grad():
-#     for iteraction, (images, labels) in tqdm(enumerate(data), total=len(data)):
-#         images, labels = images.to("cpu"), labels.to("cpu")
-#         outputs = model(images)
-#         _, predicted = torch.max(outputs.data, 1)
-#         total += labels.size(0)
-#         correct += (predicted == labels).sum().item()
-# print(timeit.default_timer() - start_time)
-# print('Accuracy of the network on the 10000 test images: %.4f %%' % (
-#     100 * correct / total))
-
-# # print(model.conv1.weight)
-
-# quanto.quantize(model, weights=quanto.qint8, activations=None)
-# quanto.freeze(model)
-
 qmodel3 = torch.load('qresnet50.pth')
 
 print(qmodel3.conv1.weight)
 
-# b = io.BytesIO()
-# torch.save(model.state_dict(), b)
-# b.seek(0)
-# state_dict2 = torch.load(b)
-
-# torch.save(model, 'qresnet50.pth')
-
-
-
-# loaded_state_dict2 = torch.load('qresnet50.pth')
-
-
-# # model2 = resnet50(pretrained=True)
-# model.load_state_dict(loaded_state_dict2)
-# print(model.conv1.weight)
 
 import timeit
 correct = 0
