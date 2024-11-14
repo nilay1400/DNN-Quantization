8ea81c53a671899f5c121c48d555a087814ab447
Samira Nazari <125339896+nilay1400@users.noreply.github.com>
2024-11-14 09:05:54 +0330

    Update alex-q-normal00001.py

--- AlexNet/8-bit/alex-q-normal00001.py
+++ AlexNet/8-bit/alex-q-normal00001.py
@@ -56,33 +56,19 @@ BER = 0.00001
 #fault_list = pandas.read_csv('resnet18_fault_list50_ber0001.csv')
 
 
-#model = vgg11_bn(pretrained=True)
-
-#model.eval()
-
 DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
                'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
-# print(torch.__version__)
-# print(DEVICE)
 
 
 train_csv = pd.read_csv('./kaggle/input/fashionmnist/fashion-mnist_train.csv')
 test_csv = pd.read_csv('./kaggle/input/fashionmnist/fashion-mnist_test.csv')
 
 
-# print(train_csv.shape)
-# print(test_csv.shape)
-
 # Customize training size here
 inputSize = 8000
 train_csv=train_csv[:inputSize]
-# len(train_csv)
-
-
-# print(train_csv.info())
-# print(train_csv.head())
 
 
 class FashionDataset(Dataset):
@@ -181,39 +167,18 @@ class fasion_mnist_alexnet(nn.Module):
 
 
 def testt(model, device, test_loader):
-    # model.eval()
-    # test_loss = 0
     correct = 0
     with torch.no_grad():
         for data, target in test_loader:
             data, target = data.to(device), target.to(device)
             output = model(data)
-            # test_loss += criterion(output, target, reduction='sum').item()
             pred = output.max(1, keepdim=True)[1]
             correct += pred.eq(target.view_as(pred)).sum().item()
 
-        # test_loss /= len(test_loader.dataset)  # loss之和除以data数量 -> mean
-        # accuracy_val.append(100. * correct / len(test_loader.dataset))
-        # print("\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n".format(
-            # test_loss, correct, len(test_loader.dataset), 100. * correct / len(test_loader.dataset)))
-        # print(test_loss)
-        # print(correct)
-        # print(accuracy_val)
         acc = 100. * correct / len(test_loader.dataset)
         return(acc)
-# print(model)
-# print(model.fc.weight._data)
-# print(model.features[25].weight._data)
-# 0, 4, 8, 11, 15, 18, 22, 25
-# print(model.classifier[0].weight)
-# 0, 3, 6
-
-# layer = 'features[0]'
 
 model = torch.load('qalex-normal.pth')
-
-# print(model)
-# print(model.conv1[0].weight._data)
 
 
 
@@ -242,78 +207,10 @@ second = ['fc1', 'fc2', 'fc3']
 
 
 
-# layer_count = 0
 original_weights = []
 for i in layer_list:
     original_weights.append(get_nested_attr(model, i).weight._data)
-    # layer_count += 1
-
-# layer_list = ['conv1', 'layer1[0].conv1', 'layer1[0].conv2', 'layer1[1].conv1', 'layer1[1].conv2', 'layer2[0].conv1', 'layer2[0].conv2', 'layer2[1].conv1', 'layer2[1].conv2', 'layer3[0].conv1', 'layer3[0].conv2', 'layer3[1].conv1', 'layer3[1].conv2', 'layer4[0].conv1', 'layer4[0].conv2', 'layer4[1].conv1', 'layer4[1].conv2', 'fc']
-#layer = random.choice(layer_list)
-#print(layer)
-#layer_weights = get_nested_attr(model, layer).weight._data.numpy()
-
-# # # Plot the distribution of weights
-#plt.hist(layer_weights.flatten(), bins=50)
-#plt.xlabel('Weight Value')
-#plt.ylabel('Frequency')
-#plt.title('Distribution of Weights')
-#plt.show()
-
-#import timeit
-#correct = 0
-#total = 0
-
-#model.eval()
-#start_time = timeit.default_timer()
-#with torch.no_grad():
- #   for iteraction, (images, labels) in tqdm(enumerate(data), total=len(data)):
- #      images, labels = images.to("cpu"), labels.to("cpu")
- #      outputs = model(images)
-  #      _, predicted = torch.max(outputs.data, 1)
-  #      total += labels.size(0)
-  #      correct += (predicted == labels).sum().item()
-#print(timeit.default_timer() - start_time)
-#print('Accuracy of the network on the 10000 test images: %.4f %%' % (
-#    100 * correct / total))
-
-    
-# # print(model.conv1.weight)
-
-#quanto.quantize(model, weights=quanto.qint8, activations=None)
-#quanto.freeze(model)
-#torch.save(model, 'vgg-q-normal.pth')
-
-# torch.load('qresnet-80-127.pth')
-
-#print(model.conv1.weight)
-
-# b = io.BytesIO()
-# torch.save(model.state_dict(), b)
-# b.seek(0)
-# state_dict2 = torch.load(b)
-
-
-# loaded_state_dict2 = torch.load('qresnet-80-127.pth')
-# model.load_state_dict(loaded_state_dict2)
-
-# import timeit
-# correct = 0
-# total = 0
-
-# model.eval()
-# start_time = timeit.default_timer()
-# with torch.no_grad():
-#     for iteraction, (images, labels) in tqdm(enumerate(data), total=len(data)):
-#         images, labels = images.to("cpu"), labels.to("cpu")
-#         outputs = model(images)
-#         _, predicted = torch.max(outputs.data, 1)
-#         total += labels.size(0)
-#         correct += (predicted == labels).sum().item()
-# print(timeit.default_timer() - start_time)
-# print('Accuracy of the golden quantized network on the 10000 test images: %.4f %%' % (
-#     100 * correct / total))
-
+  
 def no_faults():
     number =[]
  
@@ -345,24 +242,19 @@ def generate_fault_list(n):
         fault_dict['Bit'].append(bit)
 
 def test(n):
-    # model = torch.load('vgg-q-normal.pth')
     layer_count = 0
     for i in layer_list:
         get_nested_attr(model, i).weight._data = original_weights[layer_count] 
         layer_count += 1
-    # loaded_state_dict2 = torch.load('qresnet-80-127.pth')
+   
 
 
-# # model2 = resnet50(pretrained=True)
-# print(model.conv1.weight)
-    # model.load_state_dict(loaded_state_dict2)
-    # print(model.conv1.weight._data)
+
     p = 0
     for t in range(int(BER * n)):
         #layer = fault_list['Layer'][k+t]
         
         layer = random.choice(layer_list)
-        #print(layer)
         
         weights = get_nested_attr(model, layer).weight._data
         if layer in first : fi = FI(weights)
@@ -373,26 +265,15 @@ def test(n):
         new_weights = fi.inject(index, bit)
         get_nested_attr(model, layer).weight._data = new_weights
         p += 1
-        #print("which fault", k, p)
 
-#layer_weights = model.conv1.weight._data.numpy()
-
-# Plot the distribution of weights
-#plt.hist(layer_weights.flatten(), bins=50)
-#plt.xlabel('Weight Value')
-#plt.ylabel('Frequency')
-#plt.title('Distribution of Weights')
-#plt.show()
 
 
     import timeit
 
-    # model.eval()
     start_time = timeit.default_timer()
     dataloader =test_loader
     model.eval()
     acc = testt(model, DEVICE, dataloader)
-    # print(acc)ts.writerow(csv_output)
     print('Total Time:', timeit.default_timer() - start_time)
     print('Accuracy: %.4f %%' % (
     acc))
@@ -405,10 +286,7 @@ for k in range(Sufficient_no_faults):
     accuracy = test(n)
     acc_dict["Accuracy"].append(accuracy)
     acc_dict["noFI"].append(k)
-    # generate_fault_list(n)
 
-# data = pandas.DataFrame(fault_dict)
-# data.to_csv(csv_fault)
 data = pandas.DataFrame(acc_dict)
 data.to_csv(csv_acc)
 avg_accuracy = sum(acc_dict["Accuracy"])/len(acc_dict["Accuracy"])
