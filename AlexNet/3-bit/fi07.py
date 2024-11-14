792fd095994308756eb98394c3172420e20e668d
Samira Nazari <125339896+nilay1400@users.noreply.github.com>
2024-11-14 08:56:49 +0330

    Update fi07.py

--- AlexNet/3-bit/fi07.py
+++ AlexNet/3-bit/fi07.py
@@ -6,31 +6,21 @@ import struct
 class FI():
     def __init__(self, x):
         self.weight = x
-        # print(self.weight)
         dense_tensor = self.weight.to_dense()
         self.x_size, self.z_size, self.y_size, _ = dense_tensor.shape
-        # Unfold the values for the specific key and create a list of tensors
         unfolded_list = list(dense_tensor)
         unfolded_tensor = torch.cat(unfolded_list)
-        # Select a random index from the unfolded list
         self.flattened = unfolded_tensor.flatten()
         flatten_numpy = self.flattened.detach().numpy()
         param = flatten_numpy.size
         Sufficient_number_of_faults = round((31 * param) / (1 + ((31 * param) - 1) * (0.00010412328)))
         Sufficient_number_of_faults = math.floor(Sufficient_number_of_faults * 0.1)
-        # Sufficient_number_of_faults = 1
-        # fault_injection_accuracy = 0
-        # start_time = time.time()
-        # for i in range(Sufficient_number_of_faults):
 
     def param (self, x):
         self.weight = x
         dense_tensor = self.weight.to_dense()
-        # self.x_size, self.z_size, self.y_size, _ = dense_tensor.shape
-        # Unfold the values for the specific key and create a list of tensors
         unfolded_list = list(dense_tensor)
         unfolded_tensor = torch.cat(unfolded_list)
-        # Select a random index from the unfolded list
         self.flattened = unfolded_tensor.flatten()
         flatten_numpy = self.flattened.detach().numpy()
         param = flatten_numpy.size
@@ -39,10 +29,6 @@ class FI():
 
     def flip_random_bit(self, float_value, iibit):
         int_value = math.floor(float_value)
-        #print(bin(int_value))
-        # Generate a random bit position to flip
-        # bit_position = random.randint(0, 6)
-        #print(f"bit position:{iibit}")
 
         # Flip the bit
         if iibit == 7:
@@ -54,7 +40,6 @@ class FI():
             #self.flipped_value = int_value
         else:
             self.flipped_value = int_value ^ (1 << iibit)
-        #print(bin(self.flipped_value))
         return self.flipped_value
 
     def fault_position(self):
